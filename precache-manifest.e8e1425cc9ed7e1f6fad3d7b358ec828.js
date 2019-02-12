7d3f3ef2ae18de30a6aae9ee922faec52816f21f
thimothe-das <47544479+thimothe-das@users.noreply.github.com>
2019-02-12 01:16:04 +0100

    Add files via upload

---
+++ precache-manifest.e8e1425cc9ed7e1f6fad3d7b358ec828.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "deb04d65ec8d72ff4c7f",
+    "url": "/static/css/main.fd3868a9.chunk.css"
+  },
+  {
+    "revision": "deb04d65ec8d72ff4c7f",
+    "url": "/static/js/main.deb04d65.chunk.js"
+  },
+  {
+    "revision": "af81028fcf09ad8a7f16",
+    "url": "/static/js/1.af81028f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed4a23c14cd54eb130a766db75b806cd",
+    "url": "/index.html"
+  }
+];
